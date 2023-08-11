96a0e31f5b0e5f9b4182313fef4b910e282af562
Sean O'Hair <sean.ohair@intel.com>
2023-08-11 11:54:00 -0700

    feat: Scripted setup of WSL port forwarding
    Signed-off-by: Sean O'Hair <sean.ohair@intel.com>

---
+++ WSL_Port_Script.ps1
@@ -0,0 +1,12 @@
+$wsl_ip = (wsl hostname -I).split(" ")[0]
+echo "Ubuntu IP: $wsl_ip"
+
+# WSL Port Forwarding Port 22 AND 2223
+
+netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=22 connectport=22 connectaddress=$wsl_ip
+netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=2223 connectport=2223 connectaddress=$wsl_ip
+
+#  WSL Add firewall rules
+
+netsh advfirewall firewall add rule name="Open SSH Tunnel port 2223 for WSL2" dir=in action=allow protocol=TCP localport=2223
+netsh advfirewall firewall add rule name="Open SSH port 22 for WSL2" dir=in action=allow protocol=TCP localport=22
